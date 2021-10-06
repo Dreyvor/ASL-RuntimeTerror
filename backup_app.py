b63e8cc8055ba2f3abb919050ac7740b66e851aa
Dreyvor <bradley.mathez@epfl.ch>
2021-10-06 14:10:05 +0200

    Clean the SSL connection

--- backup_app.py
+++ backup_app.py
@@ -14,11 +14,6 @@ ssl_ctx.load_verify_locations(ca_cert_path)
 ssl_ctx.verify_mode = ssl.CERT_REQUIRED
 ssl_ctx.minimum_version = ssl.TLSVersion.TLSv1_3
 ssl_ctx.maximum_version = ssl.TLSVersion.TLSv1_3
-#ssl_ctx.options = ssl_ctx.maximum_version
-
-# We cannot disable TLSv1.3 but we can restrict the use of any other ciphers to the first recommended by "ssl_ctx.get_ciphers()"
-# "ECDHE-ECDSA-AES256-GCM-SHA384" is also recommended by https://www.iana.org/assignments/tls-parameters/tls-parameters.xhtml#tls-parameters-4
-#ssl_ctx.set_ciphers('ECDHE-ECDSA-AES256-GCM-SHA384')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
 	with ssl_ctx.wrap_socket(sock, server_hostname=BACKUP_SRV_IP) as ssock:
--- backup_srv.py
+++ backup_srv.py
@@ -9,8 +9,6 @@ ssl_ctx.load_verify_locations(ca_cert_path)
 ssl_ctx.verify_mode = ssl.CERT_REQUIRED
 ssl_ctx.minimum_version = ssl.TLSVersion.TLSv1_3
 ssl_ctx.maximum_version = ssl.TLSVersion.TLSv1_3
-#ssl_ctx.options = ssl_ctx.maximum_version
-
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.bind((BACKUP_SRV_IP, BACKUP_SRV_PORT))
