9eb694eccc2969d2409cb01c9d565b001f1a89f7
Mihhail Sokolov <mihhail1998@gmail.com>
2021-11-10 16:58:19 +0100

    Add certificate revocation to the CAServer

--- src/ca-server/main.py
+++ src/ca-server/main.py
@@ -10,12 +10,20 @@ class CAServer:
     def issue_certificate(self, host_name, dns_names):
         return self._ca.issue_certificate(host_name, dns_names=dns_names)
 
+    def revoke_certificate(self, host_name, common_name):
+        updated_ca = self._ca.revoke_certificate(host_name, common_name)
+        if updated_ca is None:
+            return "The certificate has already been revoked"
+        else:
+            self._ca = updated_ca
+            return "The certificate is now revoked"
+
 
 app = Flask(__name__)
 ca_server: CAServer = CAServer('ca-storage', 'Root CA')
 
 
-@app.post('/certificate')
+@app.post('/get_certificate')
 def get_certificate():
     data = request.get_json()
     cert = ca_server.issue_certificate(data['host'], data['dns_names'])
@@ -27,3 +35,10 @@ def get_certificate():
         'cert_common_name': cert.common_name
     }
     return response, 200
+
+
+@app.post('/revoke_certificate')
+def revoke_certificate():
+    data = request.get_json()
+    message = ca_server.revoke_certificate(data['host'], data['common_name'])
+    return {'message': message}, 200
