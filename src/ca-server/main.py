20a1112d69c31e31b66b8897a001ff2ff1a9d0e1
Mihhail Sokolov <mihhail1998@gmail.com>
2021-11-14 14:34:45 +0100

    Create Root and Intermediate CAs

--- src/ca-server/main.py
+++ src/ca-server/main.py
@@ -1,34 +1,46 @@
+import os.path
+
 from ownca import CertificateAuthority
 from flask import Flask, request
 
 
 class CAServer:
 
-    def __init__(self, storage_path, name):
-        self._ca = CertificateAuthority(ca_storage=storage_path, common_name=name)
+    def __init__(self, root_ca_storage_path='root-ca-storage', intermediate_ca_storage_path='intermediate-ca-storage'):
+        self._root_ca = CertificateAuthority(ca_storage=root_ca_storage_path, common_name="Root CA")
+        if not os.path.exists(os.path.join(root_ca_storage_path, 'certs', 'Intermediate CA')):
+            self._intermediate_ca = CertificateAuthority(ca_storage=intermediate_ca_storage_path, common_name="Intermediate CA", intermediate=True)
+            intermediate_ca_certificate = self._root_ca.sign_csr(self._intermediate_ca.csr, self._intermediate_ca.public_key)
+            with open(os.path.join(intermediate_ca_storage_path, 'ca.crt'), 'wb') as cert_file:
+                cert_file.write(intermediate_ca_certificate.cert_bytes)
+        self._intermediate_ca = CertificateAuthority(ca_storage=intermediate_ca_storage_path, common_name="Intermediate CA")
+        self._intermediate_ca_certificate = self._intermediate_ca.cert
 
-    def get_ca_certificate(self):
-        return self._ca.cert_bytes.decode("utf-8")
+    def get_root_ca_certificate(self):
+        return self._root_ca.cert_bytes.decode("utf-8")
 
-    def issue_certificate(self, host_name, dns_names):
-        return self._ca.issue_certificate(host_name, dns_names=dns_names)
+    def get_intermediate_ca_certificate(self):
+        return self._intermediate_ca.cert_bytes.decode("utf-8")
+
+    def issue_certificate(self, host_name, dns_names, oids):
+        return self._intermediate_ca.issue_certificate(host_name, dns_names=dns_names, oids=oids)
 
     def revoke_certificate(self, host_name, common_name):
-        self._ca.revoke_certificate(host_name, common_name)
+        self._intermediate_ca.revoke_certificate(host_name, common_name)
         return "Certificate for " + host_name + " is now revoked"
 
     def is_certificate_revoked(self, host_name):
-        return self._ca.load_certificate(host_name).revoked
+        return self._intermediate_ca.load_certificate(host_name).revoked
 
 
 app = Flask(__name__)
-ca_server: CAServer = CAServer('ca-storage', 'Root CA')
+ca_server: CAServer = CAServer()
 
 
 @app.post('/get_certificate')
 def get_certificate():
     data = request.get_json()
-    cert = ca_server.issue_certificate(data['host'], data['dns_names'])
+    cert = ca_server.issue_certificate(data['host'], data['dns_names'], data['oids'] if 'oids' in data else None)
     response = {
         'cert': cert.cert_bytes.decode("utf-8"),
         'cert_private_key': cert.key_bytes.decode("utf-8"),
@@ -50,3 +62,15 @@ def is_certificate_revoked():
     host = request.args.get('host')
     message = ca_server.is_certificate_revoked(host)
     return {'message': message}, 200
+
+
+@app.get('/root_ca_certificate')
+def get_root_ca_certificate():
+    certificate = ca_server.get_root_ca_certificate()
+    return {'certificate': certificate}, 200
+
+
+@app.get('/intermediate_ca_certificate')
+def get_intermediate_ca_certificate():
+    certificate = ca_server.get_intermediate_ca_certificate()
+    return {'certificate': certificate}, 200
