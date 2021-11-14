c3fa6bb035688c078e7ec7d6fbf5d5f64cc36110
Mihhail Sokolov <mihhail1998@gmail.com>
2021-11-14 13:37:01 +0100

    Fix some stuff

--- src/ca-server/main.py
+++ src/ca-server/main.py
@@ -7,16 +7,18 @@ class CAServer:
     def __init__(self, storage_path, name):
         self._ca = CertificateAuthority(ca_storage=storage_path, common_name=name)
 
+    def get_ca_certificate(self):
+        return self._ca.cert_bytes.decode("utf-8")
+
     def issue_certificate(self, host_name, dns_names):
         return self._ca.issue_certificate(host_name, dns_names=dns_names)
 
     def revoke_certificate(self, host_name, common_name):
-        updated_ca = self._ca.revoke_certificate(host_name, common_name)
-        if updated_ca is None:
-            return "The certificate has already been revoked"
-        else:
-            self._ca = updated_ca
-            return "The certificate is now revoked"
+        self._ca.revoke_certificate(host_name, common_name)
+        return "Certificate for " + host_name + " is now revoked"
+
+    def is_certificate_revoked(self, host_name):
+        return self._ca.load_certificate(host_name).revoked
 
 
 app = Flask(__name__)
@@ -31,7 +33,6 @@ def get_certificate():
         'cert': cert.cert_bytes.decode("utf-8"),
         'cert_private_key': cert.key_bytes.decode("utf-8"),
         'cert_public_key': cert.public_key_bytes.decode("utf-8"),
-        'cert_csr': cert.csr_bytes.decode("utf-8"),
         'cert_common_name': cert.common_name
     }
     return response, 200
@@ -41,4 +42,11 @@ def get_certificate():
 def revoke_certificate():
     data = request.get_json()
     message = ca_server.revoke_certificate(data['host'], data['common_name'])
+    return {'message': message}, 200
+
+
+@app.get('/is_certificate_revoked')
+def is_certificate_revoked():
+    host = request.args.get('host')
+    message = ca_server.is_certificate_revoked(host)
     return {'message': message}, 200
