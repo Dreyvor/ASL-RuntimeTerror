96514af2260a03244b53d4bf295c059b6c591cea
Dreyvor <bradley.mathez@epfl.ch>
2021-11-21 18:07:01 +0100

    some modifications in local_certs ==> start ca-server

--- local_certs/generate_certs.sh
+++ local_certs/generate_certs.sh
@@ -4,6 +4,7 @@
 
 #BACKUP_SRV_IP="192.168.10.20"
 BACKUP_SRV_IP="127.0.0.1"
+NAME="backup_srv"
 
 # Generate CA's private key and self-signed certificate
 gen_TLS_CA_cert() {
@@ -15,7 +16,7 @@ gen_TLS_CA_cert() {
     -nodes \
     -keyout rootCA-key.key \
     -out rootCA-cert.pem \
-    -subj "/C=CH/ST=Zurich/L=Zurich/O=ASL/OU=ProjectHS2021-ROOTCA/CN=localhost"
+    -subj "/C=CH/ST=Zurich/L=Zurich/O=ASL/OU=ProjectHS2021-ROOTCA/CN=RootCA"
 }
 
 # Generate web server's private key and certificate signing request (CSR)
@@ -25,7 +26,7 @@ gen_TLS_CSR() {
     -nodes \
     -out $1_TLS_CSR.pem \
     -keyout $1_TLS_CSR.key \
-    -subj "/C=CH/ST=Zurich/L=Zurich/O=ASL/OU=ProjectHS2021/CN=localhost"
+    -subj "/C=CH/ST=Zurich/L=Zurich/O=ASL/OU=ProjectHS2021/CN=$NAME"
 }
 
 # Use CA's private key to sign web server's CSR and get back the signed certificate
@@ -44,8 +45,8 @@ sign_TLS_CSR() {
 gen_TLS_CA_cert
 
 # Generate backup server TLS certificate and key and sign it with root CA
-gen_TLS_CSR "backup_srv"
+gen_TLS_CSR $NAME
 echo "subjectAltName=IP:$BACKUP_SRV_IP" > backup-ext.cnf
-sign_TLS_CSR "backup_srv" backup-ext.cnf
+sign_TLS_CSR $NAME backup-ext.cnf
 
 # Add lines below to generate similar certificates
